5f81dcca933dd424b7514c350a5325d7eb1ad65f
Marta Torres <98923174+zaguimiola@users.noreply.github.com>
2023-05-17 15:39:07 +0100

    fix API

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -3,77 +3,79 @@ import axios from "axios";
 
 import "./WeatherSearch.css";
 
-export default function WeatherSearch() {
-  const [city, setCity] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
+export default function WeatherSearch(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-  function displayWeather(response) {
-    setLoaded(true);
-    setWeather({
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
       temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      date: ``,
       description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
     });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    let apiKey = "13065ab195dea106282943aa8c990bea";
-    let units = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(displayWeather);
+    search();
   }
 
   function updateCity(event) {
     setCity(event.target.value);
   }
 
-  let form = (
-    <form className="search-form" onSubmit={handleSubmit}>
-      <div className="row">
-        <div className="col-9">
-          <input
-            type="search"
-            placeholder="Enter a city..."
-            className="form-control search-input"
-            id="city-input"
-            onChange={updateCity}
-          />
-        </div>
-        <div className="col-3">
-          <input
-            type="submit"
-            className="btn btn-primary w-100"
-            value="search"
-          />
-        </div>
-      </div>
-    </form>
-  );
+  function search() {
+    let apiKey = "13065ab195dea106282943aa8c990bea";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-  if (loaded) {
+  if (weatherData.ready) {
     return (
       <div className="WeatherSearch">
-        {form}
+        <form className="search-form" onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control search-input"
+                id="city-input"
+                onChange={updateCity}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                className="btn btn-primary w-100"
+                value="search"
+              />
+            </div>
+          </div>
+        </form>
         <div className="weather-info">
           <div className="row">
             <div className="col-6">
-              <h1>{city}</h1>
+              <h1>{weatherData.city}</h1>
               <ul>
                 <li>
-                  <span></span>, <span>{weather.description}</span>
+                  <span>{weatherData.date}</span>,{" "}
+                  <span>{weatherData.description}</span>
                 </li>
                 <li>
                   Humidity:
                   <span className="humidity-wind">
-                    <strong> {weather.humidity}</strong>%{" "}
+                    <strong> {weatherData.humidity}</strong>%{" "}
                   </span>
                   , Wind:
                   <span className="humidity-wind">
-                    <strong> {Math.round(weather.wind)}</strong>
+                    <strong> {Math.round(weatherData.wind)}</strong>
                     <span> km/h</span>
                   </span>
                 </li>
@@ -81,11 +83,11 @@ export default function WeatherSearch() {
             </div>
             <div className="col-lg-6 col-md-6 col-sm-8 d-flex justify-content-end">
               <div className="temperature-container">
-                <img src={weather.icon} alt={weather.description} />
+                <img src={weatherData.icon} alt={weatherData.description} />
               </div>
               <div className="temperature-unit">
                 <span className="temperature">
-                  {Math.round(weather.temperature)}
+                  {Math.round(weatherData.temperature)}
                 </span>
                 <span className="unit">ºC</span>
               </div>
@@ -95,6 +97,7 @@ export default function WeatherSearch() {
       </div>
     );
   } else {
-    return <div className="WeatherSearch">{form}</div>;
+    search();
+    return "Loading...";
   }
 }
