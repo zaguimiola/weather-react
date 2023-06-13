4793994f28368825a1e6660265a0b6d04de9a08a
Marta Torres <98923174+zaguimiola@users.noreply.github.com>
2023-06-13 16:17:09 +0100

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -6,13 +6,11 @@ export default function Footer() {
   return (
     <footer>
       This project was coded by <strong> Marta Torres</strong> and is{" "}
-      <a href="https://github.com/zaguimiola/weather-react" target="_blank">
+      <a href="https://github.com/zaguimiola/weather-react">
         open-sourced on Github
       </a>{" "}
       and{" "}
-      <a href="https://profound-babka-a6bada.netlify.app/" target="_blank">
-        hosted on Netlify
-      </a>
+      <a href="https://profound-babka-a6bada.netlify.app/">hosted on Netlify</a>
     </footer>
   );
 }
