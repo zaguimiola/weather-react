fc004ddfb2c33e474460d009212a4ada038bb6e8
Marta Torres <98923174+zaguimiola@users.noreply.github.com>
2023-06-14 12:03:28 +0100

    update weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -18,9 +18,15 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         {" "}
         <div className="row">
-          <div className="col-2">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 6) {
+              return (
+                <div className="col-2" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
