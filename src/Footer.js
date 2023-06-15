59df9a875d4c0182e05c05293f21c7df967b4e08
Marta Torres <98923174+zaguimiola@users.noreply.github.com>
2023-06-15 13:27:29 +0100

    refactored code and fixing warnings

--- src/Footer.js
+++ src/Footer.js
@@ -6,11 +6,21 @@ export default function Footer() {
   return (
     <footer>
       This project was coded by <strong> Marta Torres</strong> and is{" "}
-      <a href="https://github.com/zaguimiola/weather-react">
+      <a
+        href="https://github.com/zaguimiola/weather-react"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
         open-sourced on Github
       </a>{" "}
       and{" "}
-      <a href="https://profound-babka-a6bada.netlify.app/">hosted on Netlify</a>
+      <a
+        href="https://profound-babka-a6bada.netlify.app/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        hosted on Netlify
+      </a>
     </footer>
   );
 }
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,6 +17,14 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
+  function load() {
+    let apiKey = "13065ab195dea106282943aa8c990bea";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -37,11 +45,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "13065ab195dea106282943aa8c990bea";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    load();
 
     return null;
   }
